2d70d811da3cf8071b6841c3c6e1309ab99e7a9b
Jakkins <36565273+Jakkins@users.noreply.github.com>
2020-09-10 19:44:40 +0000

    Create Server.java

---
+++ Server1 - No Sec Layer/Server.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+    (https://www.codejava.net/java-se/networking/java-socket-server-examples-tcp-ip)
+
+    Il server parte e si mette in ascolto.
+
+    Quando arriva una richiesta fa partire un Thread.
+*/
+
+public class Server {
+
+    private int port;
+    private boolean isServerOn;
+
+    public Server(int port) {
+        this.isServerOn = true;
+        this.port = port;
+    }
+
+    public void start() {
+                    
+        // create socket
+        ServerSocket ss = null;
+        try { ss = new ServerSocket(port);
+        } catch (IOException e1) { e1.printStackTrace(); }
+
+        // SERVER LOOP
+        while(isServerOn) {
+            try {
+                // Note that the accept() method blocks the current thread until a connection is made.
+                Socket s = ss.accept();
+                new ServerThread(s).start();
+                System.out.println("Connessione accettata, mi rimetto in ascolto");
+                
+            } catch (IOException e) { e.printStackTrace(); }
+        }
+        
+        try { ss.close();
+        } catch (IOException e) { e.printStackTrace(); }
+	}
+
+}
